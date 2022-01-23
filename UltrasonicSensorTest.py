871071669a334721bab6c63be1236529a49045ed
jjoby12323 <64381188+jjoby12323@users.noreply.github.com>
2022-01-23 10:26:20 +0100

    Create UltrasonicSensorTest.py

---
+++ UltrasonicSensorTest.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+ 
+GPIO_TRIGGER = 20
+GPIO_ECHO = 21
+ 
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+      
+    TimeElapsed = StopTime - StartTime
+    distance = (TimeElapsed * 34300) / 2
+    return distance
+ 
+if __name__ == '__main__':
+    while True:
+        dist = distance()
+        print ("Measured Distance = %.1f cm" % dist)
+        time.sleep(1)
+
